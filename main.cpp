ef4ed34e87d340561657c4e4f3e9875c8544321c
reokoizumi <reokoizumi0930@gmail.com>
2021-08-21 16:42:36 +0900

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < n; i++)
+using ll = long long;
+using P = pair<int, int>;
+const int INF = 1e+8;
+
+int main() {
+  
+  return 0;
+}
