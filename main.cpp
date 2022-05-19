8d360f45b0780a397d6279e64a161249da9ddafa
reokoizumi <reokoizumi0930@gmail.com>
2022-05-19 23:34:37 +0900

    dp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@ template<class T> void chmin(T& a, T b) { if (a > b) a = b;}
 using ll = long long;
 using P = pair<int, int>;
 using Graph = vector<vector<int>>;
-const int INF = 1e+8;
+// const int INF = 1e+8;
+const long long INF = 1LL << 60;
 double PI = 3.141592653589793238;
 
 int main() {
